630053c21ea09006849cb779121fb78a45ca2dbb
palak <palak170306@gmail.com>
2026-02-07 18:19:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = " harry";
+let b= 76;
+console.log(a+b);
+console.log(typeof(a+b));
+
+const d ={
+    name: "harry",
+    section: 1,
+    isPrincipal: false
+
+}
+d['friend']= "palak";
+console.log(d);
